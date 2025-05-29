57a6d04d35c5ccb5bbea5bb1e1174fd1646b2620
Ihsan-Ahmad Olamide Sodeko <ihsan.ahmadsodeko@gmail.com>
2025-05-29 15:31:26 +0100

    Adding front page of Todo list with js

--- todo.js
+++ todo.js
@@ -1,6 +1,4 @@
 function AddTask(){
     let newList = document.createElement('li');
-    let inputContent = document.getElementById('inputBox')
-    newList.textContent = inputContent.value();
-    document.getElementById('toDoList').appendChild(newList);
+    let task = document.getElementById('inputBox');
 }
